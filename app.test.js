2ba3c652b1cff97fe747ca6f6ffa3839cf480812
Squishydup <lelegamer69005@gmail.com>
2026-06-30 09:22:52 +0200

    Update app.test.js

--- app.test.js
+++ app.test.js
@@ -1,4 +1,13 @@
 const calcul = require('./app');
+
 test('adds 1 + 2 to equal 3', () => {
   expect(calcul(1, 2)).toBe(3);
+});
+
+test('returns sum when a is greater than 10', () => {
+  expect(calcul(15, 5)).toBe(20);
+});
+
+test('returns sum when a is exactly 10', () => {
+  expect(calcul(10, 5)).toBe(15);
 });
